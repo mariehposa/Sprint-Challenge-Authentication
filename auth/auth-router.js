6807ce6afd564496631c4a124991eaf9adf34a58
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-15 08:39:02 +0000

    create register endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,7 +5,19 @@ const db = require('../jokes/jokes-model')
 
 router.post('/register', (req, res) => {
   // implement registration
-  
+  let newUser = req.body;
+  const hashPassword = bcrypt.hashSync(newUser.password, 10)
+  newUser.password = hashPassword;
+
+  db.addUser(newUser)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(err => {
+      res.status(400).json({
+        message: 'Couldnt register user' + err.message
+      })
+    })
 });
 
 router.post('/login', (req, res) => {
