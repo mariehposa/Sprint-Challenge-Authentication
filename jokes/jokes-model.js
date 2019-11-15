b40c48259a82afc3ff5b0c38311e0bb9ca4b9610
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-15 08:25:31 +0000

    create model to get all users and by id

--- jokes/jokes-model.js
+++ jokes/jokes-model.js
@@ -1,5 +1,14 @@
 const db = require('../database/dbConfig')
 
 function getUsers() {
-    return db()
+    return db('users')
+    .select('username')
 }
+
+function getUserId(id) {
+    return db('users as u')
+    .where('u.id', id)
+    .select('u.username')
+}
+
+function
